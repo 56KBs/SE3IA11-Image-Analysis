89cc5dd38ea68081edb9a43fe9cc65c33517cef5
56KBs <matt@infusioncomputing.com>
2015-10-30 15:51:05 +0000

    Initial commit
    Now with .gitignore doing its job

---
+++ ImageEnhancer/ImageEnhancerLibrary/ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ImageEnhancerLibrary
+{
+    public static class ExtensionMethods
+    {
+        public static byte ToGreyscale(this Color color)
+        {
+            return (byte) ((color.R + color.G + color.B) / 3);
+        }
+    }
+}
---
+++ ImageEnhancer/ImageEnhancerLibrary/FFT.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Numerics;
+
+namespace ImageEnhancerLibrary
+{
+    public class FFT
+    {
+        public Bitmap original { get; set; }
+
+        private List<List<Complex>> complexArray { get; set; }
+
+        private List<List<Complex>> fourierArray { get; set; }
+
+        public FFT(Bitmap original)
+        {
+            this.original = original;
+            this.complexArray = new List<List<Complex>>();
+        }
+
+        private void BuildComplexArray()
+        {
+            for (var i = 0; i < original.Width; i++)
+            {
+                for (var j = 0; j < original.Height; j++)
+                {
+                    this.complexArray[i][j] = new Complex((double) original.GetPixel(i, j).ToGreyscale(), 0);
+                }
+            }
+        }
+
+        private void RunFFT(List<List<Complex>> complexArray)
+        {
+            
+        }
+    }
+}
