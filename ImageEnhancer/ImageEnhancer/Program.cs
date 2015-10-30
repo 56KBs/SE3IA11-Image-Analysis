1f5036804143644e4f2dfe1bebcc979b6a24686e
56KBs <matt@infusioncomputing.com>
2015-10-30 15:46:58 +0000

    Revert "Revert "Initial commit""
    This reverts commit ffb69f3f252d5705c481af61b22faa605d11615b.

---
+++ ImageEnhancer/ImageEnhancer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageEnhancer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Matt's image enhancer");
+            Console.WriteLine("Specify a file to load");
+
+            var file = Console.ReadLine();
+
+
+        }
+    }
+}
