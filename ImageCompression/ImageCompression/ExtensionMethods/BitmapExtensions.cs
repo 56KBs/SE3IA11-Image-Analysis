9ae824a8d0a392a7b029e3a17212bdb887e9c2c8
56KBs <matt@infusioncomputing.com>
2015-12-06 10:38:18 +0000

    Fix image reading
    Fix image reading to ensure that the images are read correctly

--- ImageCompression/ImageCompression/ExtensionMethods/BitmapExtensions.cs
+++ ImageCompression/ImageCompression/ExtensionMethods/BitmapExtensions.cs
@@ -18,6 +18,7 @@ namespace ImageCompression.ExtensionMethods
             var pixelFormat = bitmap.PixelFormat;
 
             // NEED TO CALCULATE BYTES PER PIXEL AS SOME FORMATS HALF ALPHA CHANNEL!
+            var bytesPerPixel = bitmap.BytesPerPixel();
 
             // Make a new colour array
             var colorData = new ColorModel.RGB[bitmapWidth, bitmapHeight];         
@@ -48,7 +49,7 @@ namespace ImageCompression.ExtensionMethods
 
                         colorData[i, j] = new ColorModel.RGB(*(offsetDataPointer + 2), *(offsetDataPointer + 1), *offsetDataPointer, ColorModel.RGB.ColorDepth.TwentyFour);
 
-                        offsetDataPointer += 3;
+                        offsetDataPointer += bytesPerPixel;
                     }
                 });
             }
@@ -57,6 +58,36 @@ namespace ImageCompression.ExtensionMethods
 
             // Return the colour data
             return colorData;
+        }
+
+        public static byte BytesPerPixel(this Bitmap bitmap)
+        {
+            if (bitmap.PixelFormat == PixelFormat.Canonical ||
+                bitmap.PixelFormat == PixelFormat.Format32bppArgb ||
+                bitmap.PixelFormat == PixelFormat.Format32bppPArgb ||
+                bitmap.PixelFormat == PixelFormat.Format32bppRgb)
+            {
+                return 32 / 8;
+            }
+            else if (bitmap.PixelFormat == PixelFormat.Format24bppRgb)
+            {
+                return 24 /8;
+            }
+            else if (bitmap.PixelFormat == PixelFormat.Format16bppArgb1555 ||
+                     bitmap.PixelFormat == PixelFormat.Format16bppGrayScale ||
+                     bitmap.PixelFormat == PixelFormat.Format16bppRgb555 ||
+                     bitmap.PixelFormat == PixelFormat.Format16bppRgb565)
+            {
+                return 16 / 8;
+            }
+            else if (bitmap.PixelFormat == PixelFormat.Format8bppIndexed)
+            {
+                return 8 /8;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Unhandled bitmap pixelformat");
+            }
         }
     }
 }
